be5957b9233cadeea4aaea8c4f8485459b03ffd3
Nikolay Vorotnev <vrtnev@tltdev.ru>
2020-05-25 17:10:07 +0400

    Coded category controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -12,7 +13,9 @@ class MainController extends Controller
     public function categories() {
         return view('categories');
     }
-    public function category($category) {
+    public function category($code) {
+        $category = Category::where('code', $code)->first();
+        dd($category);
         return view('category', compact('category'));
     }
     public function product($product = null) {
