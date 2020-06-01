a2fd0e9fb60d680e13a02f77bdadbcd8b20f7ccb
Nikolay Vorotnev <vrtnev@tltdev.ru>
2020-06-01 15:01:52 +0400

    Added bootstrap template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.master')
 
-    @section('title', 'Главная страница')
+@section('title', 'Главная страница')
 
-    @section('content')
-        <h1>Shop by vrtnev</h1>
-        @foreach($products as $product)
+@section('content')
+    @foreach($products as $product)
+        @if($loop->index % 2 == 0)
+            <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
             @include('layouts.card', compact('product'))
-        @endforeach
-    @endsection
+            @if($loop->remaining == 0)
+                </div>
+            @endif
+        @endif
+        @if($loop->index % 2 != 0)
+            @include('layouts.card', compact('product'))
+            </div>
+        @endif
+    @endforeach
+@endsection
--- resources/views/layouts/card.blade.php
+++ resources/views/layouts/card.blade.php
@@ -1,11 +1,12 @@
-<div style="border: 2px solid black; width: 200px;">
-    <img style="width: 100%"
-         src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-se-family-select-2020?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586794486444">
-    <h2>{{ $product->name }}</h2>
-    <p>{{ $product->price }} руб.</p>
-    <form action="{{ route('cart-add', $product)  }}" method="post">
-        <button type="submit">Добавить в корзину</button>
-        <a href="{{route('product', [$product->category->code, $product->code])}}">Подробнее</a>
-        @csrf
-    </form>
+<div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+    <div class="my-3 py-3">
+        <h2 class="display-5">{{ $product->name }}</h2>
+        <p class="lead">{{ $product->price }} руб.</p>
+        <form action="{{ route('cart-add', $product)  }}" method="post">
+            <button class="btn btn-outline-secondary" type="submit">Добавить в корзину</button>
+            <a class="btn btn-outline-secondary" href="{{route('product', [$product->category->code, $product->code])}}">Подробнее</a>
+            @csrf
+        </form>
+    </div>
+    <div class="bg-light shadow-sm mx-auto" style="background-image: url('https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-se-family-select-2020?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586794486444'); background-repeat: no-repeat; background-position: center; background-size: auto 100%; width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
 </div>
