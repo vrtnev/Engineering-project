6075efb379e08ee64758732d59bdacaa293f9f63
Nikolay Vorotnev <vrtnev@tltdev.ru>
2020-06-29 03:45:20 +0300

    Исправлено отображение изображений

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -18,21 +18,24 @@ class ProductsTableSeeder extends Seeder
                 'name' => 'Apple iPhone 11 Pro Max 256 ГБ тёмно-зелёный',
                 'code' => 'iphone-11-pro-max-256-midnight-green',
                 'description' => 'Apple возвращается с тремя новыми iPhone - ежегодная церемония повторяется. Самым большим и увесистым среди новинок является iPhone 11 Pro Max - в первую очередь он будет интересен тем, кому нравятся экраны побольше и высококачественные камеры.',
-                'price' => '113990'
+                'price' => '113990',
+                'image' => 'products/iphone-11-pro-max-256-midnight-green.png'
             ],
             [
                 'category_id' => '2',
                 'name' => 'Apple iPad Pro (2020) 12,9" Wi-Fi + Cellular 128 ГБ, «серый космос»',
                 'code' => 'ipad-pro-2020-12.9-cellular-128-space-gray',
                 'description' => 'Ваш будущий компьютер теперь не компьютер.Он быстрее многих ноутбуков. У него камеры уровня Pro и сканер LiDAR. А ещё им можно управлять с помощью трекпада.',
-                'price' => '100990'
+                'price' => '100990',
+                'image' => 'products/iphone-11-pro-max-256-midnight-green.png'
             ],
             [
                 'category_id' => '3',
                 'name' => 'Наушники AirPods Pro',
                 'code' => 'airpods-pro',
                 'description' => 'Это наушники совершенно нового класса. AirPods Pro исключительно комфортны, в них реализована технология активного шумоподавления, и можно даже выбрать вкладыши своего размера. Вы будете лучше чувствовать музыку, а не наушники.',
-                'price' => '19990'
+                'price' => '19990',
+                'image' => 'products/iphone-11-pro-max-256-midnight-green.png'
             ]
         ]);
     }
--- resources/views/layouts/card.blade.php
+++ resources/views/layouts/card.blade.php
@@ -1,4 +1,5 @@
 <div class="card" style="width: 18rem; min-width: 18rem; margin: auto; min-height: 28rem; height: 28rem;">
+{{--    <img src="{{ Storage::url($product->image) }}" class="card-img-top" style="max-width: 100%" alt="...">--}}
     <img src="{{ Storage::url($product->image) }}" class="card-img-top" style="max-width: 100%" alt="...">
     <div class="card-body" style="display: flex; flex-direction: column; justify-content: flex-end;">
         <h5 class="card-title">{{ $product->name }}</h5>
